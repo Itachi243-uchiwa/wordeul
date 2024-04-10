5f7a91a654e0a9d0143a41fd6c3526f129485be1
63888 <63888@etu.he2b.be>
2024-04-10 10:55:24 +0200

    projet

--- projet-g63888/js/wordeul.js
+++ projet-g63888/js/wordeul.js
@@ -2,3 +2,30 @@
 
 const gameEl = document.getElementById("game");
 console.log(gameEl);
+
+function setLetter(numRow, numTile, letter) {
+    const row = gameEl.children[numRow - 1]; 
+    
+    // Sélectionner la tuile dans la ligne
+    const tile = row.children[numTile-1];
+    
+    // Modifier le contenu de la tuile avec la lettre spécifiée
+    tile.textContent = letter;
+}
+
+function keyUpHandler(event) {
+    
+    const touche = event.key;
+    
+    // Vérifie si la touche pressée est une lettre de l'alphabet
+    const alphabetLetters = /^[a-zA-Z]$/;
+    if (!alphabetLetters.test(touche)) {
+        
+        return;
+    }
+    
+    setLetter(1, 1, touche.toUpperCase()); // Convertir en majuscule
+}
+
+document.addEventListener('keyup', keyUpHandler);
+
